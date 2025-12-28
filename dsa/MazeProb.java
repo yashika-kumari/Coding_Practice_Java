cd48bdb48b48fb98750f987701d492ecfc8f6b5c
Yashika Kumari <2024pceacsyashika186@poornima.org>
2025-12-28 21:34:41 +0530

    New Problems

---
+++ dsa/MazeProb.java
@@ -0,0 +1,20 @@
+package com.company.dsa;
+
+public class MazeProb {
+    public static void main(String[] args) {
+        path("", 3, 3);
+    }
+
+    static void path(String p, int r, int c){
+        if (r ==1 && c ==1){
+            System.out.println(p);
+            return;
+        }
+        if (r > 1){
+            path( p + 'D', r - 1, c);
+        }
+        if (c > 1){
+            path( p + 'R', r, c - 1);
+        }
+    }
+}
